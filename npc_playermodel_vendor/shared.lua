fd6a23c526a9b218e9b1a24c89cd3975ce32cefd
JamieL25 <cfgmenu@gmail.com>
2025-05-06 22:12:59 +0000

    Add files via upload

---
+++ npc_playermodel_vendor/shared.lua
@@ -0,0 +1,13 @@
+-- NPC Player Model Vendor Entity
+-- shared.lua - Shared code
+-- Updated: 2025-05-06 21:19:33 by JamieL25
+
+ENT.Type = "ai"
+ENT.Base = "base_ai"
+ENT.PrintName = "Player Model Vendor"
+ENT.Author = "JamieL25"
+ENT.Category = "NPCs"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+
+ENT.AutomaticFrameAdvance = true
